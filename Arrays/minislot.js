f33cf3239d439abce72635d336932227bddc048b
LonlyGamerX <49322497+LonlyGamerX@users.noreply.github.com>
2020-06-26 16:20:02 +0200

    Added new slots

---
+++ Arrays/minislot.js
@@ -0,0 +1,3 @@
+export var miniSlot1 = ["Adaptive force", "Attack speed", "CDR"]
+export var miniSlot2 = ["Adaptive force", "Armor", "Magic Resist"]
+export var miniSlot3 = ["Health", "Armor", "Magic Resist"]
